6cf6d19b8caf4cb75a254ac1e9c445d4698d515e
jfuentes-gapsi <jfuentes@grupoasesores.com.mx>
2025-10-14 16:51:16 -0600

    last prueba
    last prueba

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_storage_bucket" "bkt-ghevents-terraform" {
+resource "google_storage_bucket" "my-bucket" {
   name                     = "bkt-demo-001"
   location                 = "us-central1"
   project                  = "project-001-jfuentes"
