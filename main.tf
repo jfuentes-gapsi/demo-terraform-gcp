e6b2ad6d78718de9c0c9dd0d857b1dbce2d4b4e7
jfuentes-gapsi <jfuentes@grupoasesores.com.mx>
2025-10-14 16:30:50 -0600

    jfuentes
    sha

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "google_storage_bucket" "bkt-ghevents-terraform" {
-  name          = "bkt-demo-001"
-  location      = "us-central1"
-  project = "project-001-jfuentes"
-  force_destroy = true
+  name                     = "bkt-demo-001"
+  location                 = "us-central1"
+  project                  = "project-001-jfuentes"
+  force_destroy            = true
   public_access_prevention = "enforced"
 }
