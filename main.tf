47a144430ca9e033d4defeace9fcec88280ba278
jfuentes-gapsi <jfuentes@grupoasesores.com.mx>
2025-10-14 22:32:37 -0600

    1
    1

--- main.tf
+++ main.tf
@@ -1,7 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "project-001-jfuentes"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
 resource "google_storage_bucket" "bkt-jfuentes14102025-003" {
   name                     = "bkt-demo-001"
-  location                 = "us-central1"
-  project                  = "project-001-jfuentes"
+  location                 = "EU"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
